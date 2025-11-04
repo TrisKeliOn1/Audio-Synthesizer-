62a5af27b86b4492837b32db717bce4660eb9603
hebert <hebertLandim007@outlook.com>
2025-11-04 15:55:48 -0300

    feat: add initial Swing window for Synthesizer

---
+++ src/com/synth/Main.java
@@ -0,0 +1,7 @@
+package com.synth;
+
+public class Main {
+    public static void main(String[] args) {
+        new SynthesizerRemastered();
+    }
+}
---
+++ src/com/synth/SynthesizerRemastered.java
@@ -0,0 +1,16 @@
+package com.synth;
+
+import javax.swing.*;
+
+public class SynthesizerRemastered {
+    private JFrame frame = new JFrame ("Synthesizer Remastered");
+
+    SynthesizerRemastered() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(613, 357);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
