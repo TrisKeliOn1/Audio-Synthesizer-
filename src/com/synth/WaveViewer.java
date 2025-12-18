a0497d96b67a13d5678c5c2f3a8849e62f642dcb
hebert <hebertLandim007@outlook.com>
2025-12-18 20:35:25 -0300

    fix(waveviewer): separate mixing and normalization

--- src/com/synth/WaveViewer.java
+++ src/com/synth/WaveViewer.java
@@ -16,26 +16,55 @@ public class WaveViewer extends JPanel {
     }
 
     @Override
-    public void paintComponent(Graphics graphics) {
+    protected void paintComponent(Graphics graphics) {
         final int PAD = 25;
         super.paintComponent(graphics);
-        Graphics2D graphics2D = (Graphics2D) graphics;
-        int numSamples = getWidth() - PAD * 2;
+
+        Graphics2D g2d = (Graphics2D) graphics;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int numSamples = Math.max(1, getWidth() - PAD * 2);
         double[] mixedSamples = new double[numSamples];
-        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // sem normalizar
         for (Oscillator oscillator : oscillators) {
             double[] samples = oscillator.getSampleWaveForm(numSamples);
             for (int i = 0; i < samples.length; i++) {
-                mixedSamples[i]  += samples[i] / oscillators.length;
+                mixedSamples[i] += samples[i];
             }
         }
+
+        // Normalização (vistual)
+        double max = 0.0;
+        for (double v : mixedSamples) {
+            max = Math.max(max, Math.abs(v));
+        }
+
+        if (max > 0.0) {
+            double scale = max > 1.0 ? max : 1.0;
+
+            for (int i = 0; i < mixedSamples.length; i++) {
+                mixedSamples[i] /= scale;
+            }
+        }
+
+
         int midY = getHeight() / 2;
-        Function<Double, Integer> sampleToYCoord = sample -> (int) (midY + sample * (midY - PAD));
-        graphics2D.drawLine(PAD, midY, getWidth() - PAD, midY);
-        graphics2D.drawLine(PAD, PAD, PAD, getHeight() - PAD);
-        for (int i = 0; i < numSamples; i++) {
-            int nextY = i == numSamples - 1 ? sampleToYCoord.apply(mixedSamples[i]): sampleToYCoord.apply(mixedSamples[i + 1]);
-            graphics2D.drawLine(PAD + i, sampleToYCoord.apply(mixedSamples[i]), PAD + i + 1, nextY);
+        Function<Double, Integer> sampleToY =
+                sample -> (int) (midY + sample * (midY - PAD));
+
+        // Eixos
+        g2d.drawLine(PAD, midY, getWidth() - PAD, midY);
+        g2d.drawLine(PAD, PAD, PAD, getHeight() - PAD);
+
+        // Desenho da onda
+        for (int i = 0; i < numSamples - 1; i++) {
+            g2d.drawLine(
+                    PAD + i,
+                    sampleToY.apply(mixedSamples[i]),
+                    PAD + i + 1,
+                    sampleToY.apply(mixedSamples[i + 1])
+            );
         }
     }
 }
